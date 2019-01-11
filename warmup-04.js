ab6edbf38ed48f7e80b12671e8d73dc721ba1864
CaityHeath <caity.heath@gmail.com>
2019-01-11 00:36:08 -0800

    promises no longer returnig undefined

--- warmup-04.js
+++ warmup-04.js
@@ -3,7 +3,6 @@
 const superagent =  require('superagent');
 
 let fetchPeopleAsync = () => {
-
   return superagent
   .get('http://swapi.co/api/people')
   .then((response) => {
@@ -26,3 +25,37 @@ let fetchPeopleAsync = () => {
 })
 .catch(err => console.err);
 }
+
+////////////////////////////////////////
+
+let fetchPeopleSync = async () => {
+  try{
+
+    let result = await superagent.get('https://swapi.co/api/people');
+    //console.log(collection.body.results);
+    let collection = result.body.results;
+    let names = [];
+
+
+    let requested = collection.map((data) => {
+      return superagent.get(data.url);
+    })
+    //console.log(requested);
+
+    let urls = await Promise.all(requested)
+    .then(people => {
+      people.forEach(data => {
+        //console.log(data);
+        names.push(data.body.name);
+      });
+
+    })
+    return names;
+  }
+
+    catch(error){console.err};
+}
+
+fetchPeopleAsync().then(char => console.log('with promises', char));
+
+fetchPeopleSync().then(char => console.log('with callbacks', char));
